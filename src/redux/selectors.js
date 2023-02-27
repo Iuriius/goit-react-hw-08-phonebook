89def9c374800d2ddba41b77996b20d204e0def8
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-27 12:41:31 +0200

    1

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContact = state => state.contacts.contacts.items;
+export const selectContact = state => state.contacts.items;
 export const selectFilter = state => state.filter.filter;
-export const selectIsLoading = state => state.contacts.contacts.isLoading;
-export const selectError = state => state.contacts.contacts.error;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
 
 export const selectVisibleContacts = createSelector(
   [selectContact, selectFilter],
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { filterReducer } from './filterSlice.js';
-import { contactReducer } from './contactSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { filterReducer } from './contacts/slices/filterSlice';
+import { contactReducer } from './contacts/slices/contactsSlice';
+import { authReducer } from './auth/authSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: contactReducer,
     filter: filterReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
