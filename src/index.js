04116199bb51ce52f9eba36c1303767ec13e032b
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-27 12:47:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'modern-normalize/modern-normalize.css';
+import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import 'modern-normalize/modern-normalize.css';
-import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
